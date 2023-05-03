ad06e271990338b5042c8ae93b8cf678387a16f2
Henrique Ramos <78941789+hqrms@users.noreply.github.com>
2023-05-03 09:36:37 -0300

    Update epd_v2.py

--- epd_v2.py
+++ epd_v2.py
@@ -26,8 +26,34 @@ class DisplayManager():
         self.epd.init(self.display_mode)
         logging.info("Display Init")
 
+    def update_display(self, board):
+        self.epd.display_4Gray(epd.getbuffer_4Gray(board))
+
+
+class DrawingBoard():
+    def __init__(self, height = 480, width = 280, color = 0xFF): 
+        self.height = height
+        self.width = width
+        self.color = color 
+        self.greyscale = "L"
+        self.board = Image.new(self.greyscale, (self.height, self.width), self.color)
+
+    def paste_image(self, image, position):
+        x,y = position
+        self.board.paste(image, (x,y))
+
+
 epd = DisplayManager()
+canvas = DrawingBoard()
 
 epd.init_display()
 epd.clear_display()
+
+img = Image.open("/home/epaper/test2.png")
+img = img.rotate(90, PIL.Image.NEAREST, expand = 1)
+img = img.resize((200, 100))
+
+canvas.paste_image(img, [280,0])
+epd.update_display(canvas.board)
+
 
